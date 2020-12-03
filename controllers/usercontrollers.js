b50570189cb2cb02fcd39ce18de3e5c953505797
subhankr12 <subhankr12@gmail.com>
2020-12-03 17:04:55 +0530

    create route and its controllers

---
+++ controllers/usercontrollers.js
@@ -0,0 +1,9 @@
+
+
+module.exports.register = (req, res) => {
+    return res.end('<h1>Register Page</h1>')
+}
+
+module.exports.login = (req, res) => {
+  return res.end('<h1>Login Page</h1>')
+}
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes/user'));
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on PORT ${PORT}`);
