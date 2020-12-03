e2ea87b30c40b68fabaefa848825064efaf08217
subhankr12 <subhankr12@gmail.com>
2020-12-03 17:24:31 +0530

    connect to mongoose

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://subhankr12:helloworld@cluster0.c6pnc.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Successfully connected to MongoDB');
+})
 
 app.use(require('./routes/user'));
 
